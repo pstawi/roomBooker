d95a7a71cff3c15dc12bc404d77ac025f4484ec8
pierreStawi <pstawikowski@fcdigital.fr>
2025-09-30 12:05:00 +0200

    feat: Implement authentication and registration features
    - Added LoginForm and RegisterForm components for user authentication.
    - Created useAuth hook to manage authentication state and token handling.
    - Integrated axios for API calls to handle user login and registration.
    - Updated Header component to include logout functionality.
    - Modified router to include login and registration routes.
    - Enhanced error handling for login and registration processes.
    - Added CORS support in the backend for cross-origin requests.

--- back/index.js
+++ back/index.js
@@ -3,9 +3,12 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/authRoutes.js";
 import postRoutes from "./routes/postRoutes.js";
 import typeRoutes from "./routes/typeRoutes.js";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
--- front/src/router.jsx
+++ front/src/router.jsx
@@ -1,15 +1,20 @@
 import { createBrowserRouter } from "react-router";
 import Layout from "./Layout/Layout";
 import Home from "./views/Home";
+import Login from "./views/Login";
 
 const router = createBrowserRouter([
     {
-        path : "/",
-        element : <Layout />,
-        children : [
+        path: "/",
+        element: <Layout />,
+        children: [
             {
-                path : "/",
-                element : <Home />
+                path: "/",
+                element: <Home />
+            },
+            {
+                path: "/Login",
+                element: <Login />
             }
         ]
     }
